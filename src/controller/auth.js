38f1c0f6da6c9cc4ee0343849f4ee6bfbefa1c5d
caglayan <caglayanustaa@gmail.com>
2026-06-06 14:20:35 +0300

    register cookie added

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -8,6 +8,14 @@ import { ONE_DAY } from "../constants/index.js";
 
 export const registerController = async (req, res) => {
   const { users, session } = await registerService(req.body);
+  res.cookie("refreshToken", session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie("sessionId", session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
 
   res.status(201).json({
     message: "Register successfully",
